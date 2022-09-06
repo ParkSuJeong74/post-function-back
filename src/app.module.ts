a4d174f4ea20e2527237940d3c73c2579259bef0
ParkSuJeong74 <sujeong9158@gmail.com>
2022-09-06 12:36:21 +0900

    feat: user, post module 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { validation } from './commons/utils';
+import { UserModule } from './user/user.module';
+import { PostModule } from './post/post.module';
 
 @Module({
   imports: [
@@ -14,6 +16,8 @@ import { validation } from './commons/utils';
       isGlobal: true,
       validationSchema: validation,
     }),
+    UserModule,
+    PostModule,
   ],
 })
 export class AppModule {}
