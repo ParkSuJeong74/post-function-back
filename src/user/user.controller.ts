e888e259d638bd52baa9e21f7ed63a83e6b3507f
ParkSuJeong74 <sujeong9158@gmail.com>
2022-09-06 13:59:33 +0900

    feat: create user api swagger 적용

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,27 @@
-import { Body, Controller, Logger, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Logger, Post } from '@nestjs/common';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { User } from '@prisma/client';
 import { CreateUserDto, CreateUserResponseDto } from './dto';
 import { UserService } from './user.service';
 
+@ApiTags('User API')
 @Controller('user')
 export class UserController {
   private readonly logger = new Logger(`UserController`);
   constructor(private readonly userService: UserService) {}
 
+  @HttpCode(201)
   @Post()
+  @ApiOperation({
+    summary: '회원 생성 API',
+    description: '이름으로 회원을 생성한다.',
+  })
+  @ApiResponse({
+    status: 201,
+    description: '회원 생성 성공',
+    type: CreateUserResponseDto,
+  })
+  @ApiBody({ type: CreateUserDto })
   async createUser(
     @Body() createUserDto: CreateUserDto,
   ): Promise<CreateUserResponseDto> {
